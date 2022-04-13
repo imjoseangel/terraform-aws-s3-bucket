e6a543dc7eb25e598ac9fded6f86c7fb4e8ec8bb
imjoseangel <josea.munoz@gmail.com>
2022-04-13 09:40:48 +0200

    add arn

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "terraform_bucket" {
   description = "The state_bucket name"
   value       = aws_s3_bucket.main.bucket
 }
+
+output "terraform_bucket_arn" {
+  description = "The state_bucket name"
+  value       = aws_s3_bucket.main.arn
+}
